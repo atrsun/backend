2c9968473200022f60a2ea694888483407f3544a
Ali Ghorbani <ali.hmd.programmer@gmail.com>
2025-11-18 23:20:30 +0330

    debug login

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,37 @@
 import {
 //   ClassSerializerInterceptor,
-  HttpStatus,
-  UnprocessableEntityException,
+//   HttpStatus,
+//   UnprocessableEntityException,
   ValidationPipe,
 } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 // import { NestFactory, Reflector } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+// import { Transport } from '@nestjs/microservices';
 import type { NestExpressApplication } from '@nestjs/platform-express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import compression from 'compression';
-import helmet from 'helmet';
-import morgan from 'morgan';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import compression from 'compression';
+// import helmet from 'helmet';
+// import morgan from 'morgan';
 import { AppModule } from './app.module.ts';
 // import { HttpExceptionFilter } from './filters/bad-request.filter.ts';
-import { setupSwagger } from './setup-swagger.ts';
-import { ApiConfigService } from './shared/services/api-config.service.ts';
-import { SharedModule } from './shared/shared.module.ts';
+// import { setupSwagger } from './setup-swagger.ts';
+// import { ApiConfigService } from './shared/services/api-config.service.ts';
+// import { SharedModule } from './shared/shared.module.ts';
 import cookieParser from 'cookie-parser';
 
 export async function bootstrap(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
-    new ExpressAdapter(),
     { cors: {
         origin: "*"
     } },
   );
 //   app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-  app.use(helmet());
+//   app.use(helmet());
   app.use(cookieParser());
   // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
-  app.use(compression());
-  app.use(morgan('full'));
+//   app.use(compression());
+//   app.use(morgan('full'));
 //   app.enableVersioning();
 
 //   const reflector = app.get(Reflector);
@@ -44,41 +43,41 @@ export async function bootstrap(): Promise<NestExpressApplication> {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    //   errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
       transform: true,
-      dismissDefaultMessages: true,
-      exceptionFactory: (errors) => new UnprocessableEntityException(errors),
+    //   dismissDefaultMessages: true,
+    //   exceptionFactory: (errors) => new UnprocessableEntityException(errors),
     }),
   );
 
-  const configService = app.select(SharedModule).get(ApiConfigService);
+//   const configService = app.select(SharedModule).get(ApiConfigService);
 
   // only start nats if it is enabled
-  if (configService.natsEnabled) {
-    const natsConfig = configService.natsConfig;
-    app.connectMicroservice({
-      transport: Transport.NATS,
-      options: {
-        url: `nats://${natsConfig.host}:${natsConfig.port}`,
-        queue: 'main_service',
-      },
-    });
+//   if (configService.natsEnabled) {
+    // const natsConfig = configService.natsConfig;
+    // app.connectMicroservice({
+    //   transport: Transport.NATS,
+    //   options: {
+        // url: `nats://${natsConfig.host}:${natsConfig.port}`,
+        // queue: 'main_service',
+    //   },
+    // });
 
-    await app.startAllMicroservices();
-  }
+    // await app.startAllMicroservices();
+//   }
 
-  if (configService.documentationEnabled) {
-    setupSwagger(app);
-  }
+//   if (configService.documentationEnabled) {
+//     setupSwagger(app);
+//   }
 
-  // Starts listening for shutdown hooks
-  if (!configService.isDevelopment) {
-    app.enableShutdownHooks();
-  }
+//   // Starts listening for shutdown hooks
+//   if (!configService.isDevelopment) {
+//     app.enableShutdownHooks();
+//   }
 
-  const port = configService.appConfig.port;
+//   const port = configService.appConfig.port;
 
-  await app.listen(port, "0.0.0.0");
+  await app.listen(3000);
   console.info(`server running on ${await app.getUrl()}`);
 
   return app;
